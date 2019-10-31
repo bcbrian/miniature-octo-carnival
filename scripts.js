82a3b23723525b7b834fc20fa5135f6d5537496b
Brian Bartholomew <me@bcbrian.com>
2019-10-30 21:02:16 -0500

    yo we did this boom! turn 'em on and off

--- scripts.js
+++ scripts.js
@@ -1,5 +1,48 @@
-function startRide() {
-  const inputEl = document.getElementById("action");
+function startMiniOctoCarnival() {
+  const rides = [
+    "carousel",
+    "ferris wheel",
+    "twirl and hurl",
+    "bumper cars",
+    "swing ride",
+    "log flume",
+    "sky lift",
+    "gravitron"
+  ];
+  /* <div id="carousel" class="ride">
+    Carousel
+  </div> */
+  const ridePanelEl = document.getElementById("ride-panel");
+  for (let i = 0; i < rides.length; i++) {
+    const ride = rides[i];
+    const rideEl = document.createElement("div");
+    rideEl.setAttribute("id", ride);
+    rideEl.setAttribute("data-js-selector", "ride");
+    rideEl.classList.add("ride");
+    rideEl.innerHTML = ride;
+    console.log(rideEl);
+    ridePanelEl.append(rideEl);
+  }
 
-  console.log(inputEl.value);
+  function startRide() {
+    const inputEl = document.getElementById("action");
+    const action = inputEl.value;
+    console.log(action);
+
+    const rideEls = document.querySelectorAll("[data-js-selector='ride']");
+
+    rideEls.forEach(function(rideEl) {
+      console.log(rideEl);
+      const id = rideEl.getAttribute("id");
+
+      if (action.toLowerCase() === id.toLowerCase()) {
+        rideEl.classList.toggle("active");
+      } else {
+        console.log("Yikes not sure what you wanted to do there?");
+      }
+    });
+  }
+
+  window.startRide = startRide;
 }
+startMiniOctoCarnival();
